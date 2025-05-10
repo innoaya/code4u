0b27a8fc76bc0a127dfdfb34f61f640ea4480639
Aung Kyaw Minn <ajminn207@gmail.com>
2025-05-10 15:28:06 +0630

    remove all console logs in production mode

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,32 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import terser from '@rollup/plugin-terser'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [
     vue(),
-    vueDevTools(),
-  ],
+    // Only enable devTools in development mode
+    command === 'serve' ? vueDevTools() : null,
+    // Use terser for production builds
+    command === 'build' ? terser() : null,
+  ].filter(Boolean),
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-})
+  // Production build optimizations
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        // Remove console.log in production
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ['console.log', 'console.info', 'console.debug']
+      }
+    }
+  },
+}))
